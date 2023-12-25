909a0452489fdd8af883f38a80ba53c441e4cace
unknown <t.ben-arfa@cliqeo.com>
2023-12-25 18:03:10 +0100

    fixed  page  contact

--- app/api/send-email.js
+++ app/api/send-email.js
@@ -1,15 +1,30 @@
-const nodemailer = require('nodemailer');
+// pages/api/send-email.js
+import nodemailer from 'nodemailer';
 
-const sendEmail = async (req, res) => {
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).end(); // Method Not Allowed
+    }
+
+    const { to, subject, body } = req.body;
+
     try {
         // Your email sending logic using nodemailer
-        // ...
+        const transporter = nodemailer.createTransport({
+            // Your email configuration goes here
+            // ...
+        });
+
+        await transporter.sendMail({
+            from: 'your_email@gmail.com',
+            to: to,
+            subject: subject,
+            text: body,
+        });
 
         res.status(200).json({ message: 'Email sent successfully' });
     } catch (error) {
         console.error('Error sending email:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-};
-
-module.exports = { sendEmail };
+}
